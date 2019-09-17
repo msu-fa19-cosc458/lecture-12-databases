a44ff0c1d15da11bf0cf91f8014def06e9d6757c
Phil B <philanaaurelia@gmail.com>
2019-09-17 17:28:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,17 @@ import os, flask
 
 app = flask.Flask(__name__)
 
+import models # this needs to be here 
+
 @app.route('/')
 def index():
     return 'Hello, world!'
 
-app.run(
-    host=os.getenv('IP', '0.0.0.0'),
-    port=int(os.getenv('PORT', 8080)),
-    debug=True
-)
+if __name__ == '__main__': 
+    app.run(
+        host=os.getenv('IP', '0.0.0.0'),
+        port=int(os.getenv('PORT', 8080)),
+        debug=True
+    )
 
 
